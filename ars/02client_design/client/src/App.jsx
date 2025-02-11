f13ddc38ff659b7c14b5cad927336d140486eea7
Poojitha Ms <poojithanamu@gmail.com>
2025-02-11 11:03:33 +0530

    react

---
+++ ars/02client_design/client/src/App.jsx
@@ -0,0 +1,16 @@
+import Pageheader from "./header/Pageheader"
+
+
+function App() {
+
+  return (
+    <>
+    <Pageheader/>
+     <h1>hello world</h1>
+     <p>this is poojitha</p>
+     
+    </>
+  )
+}
+
+export default App
